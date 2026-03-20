bd0077122cfd9e28669a2dbcb4e3e6d9ffcbaa71
DESKTOP-8M5NE0D\ADMIN <doanvanhungcntt@gmail.com>
2026-03-20 19:17:12 +0700

    add jenkins file

--- tests/testcases/test_login.py
+++ tests/testcases/test_login.py
@@ -9,7 +9,10 @@ def test_open_homepage(home_page):
 
     home_page.click_login_button()
 
-# @pytest.mark.smoke
-# def test_open_homepage2(login_page):
+@allure.feature("Tài sản đảm bảo")
+@allure.story('Quản lý danh mục DLT Tài sản')
+@allure.title("TC022 - 01: Quản lý danh mục đơn vị định giá - Kiểm tra Thêm mới")
+@pytest.mark.regression
+def test_open_homepage2(home_page):
 
-#     assert page.title() != ""
+    home_page.click_login_button()
